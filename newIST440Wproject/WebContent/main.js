73efe8a9a9f2ff7f5e0bcc4f90793bc23525c041
avlita <37907348+avlita@users.noreply.github.com>
2018-04-24 00:32:53 -0400

    New changes
    added translate.js , css file, and updated the index.html. The translation isn't working yet

--- newIST440Wproject/WebContent/main.js
+++ newIST440Wproject/WebContent/main.js
@@ -1,35 +1,70 @@
-function uploadFiles(event) {
-  event.stopPropagation(); // Stop stuff happening
-  event.preventDefault(); // Totally stop stuff happening
+'use strict';
 
-  //Grab the file and asynchronously convert to base64.
-  var file = $('#fileInput')[0].files[0];
-  var reader = new FileReader()
-  reader.onloadend = processFile
+var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?fields=responses(fullTextAnnotation%2Ftext%2CtextAnnotations%2Flocale)&key=' + window.apiKey;
+
+$(function () {
+  $('#fileform').on('submit', uploadFiles);
+});
+
+/**
+ * 'submit' event handler - reads the image bytes and sends it to the Cloud
+ * Vision API.
+ */
+function uploadFiles (event) {
+  event.preventDefault(); // Prevent the default form post
+
+  // Grab the file and asynchronously convert to base64.
+  var file = $('#fileform [name=fileField]')[0].files[0];
+  var reader = new FileReader();
+  reader.onloadend = processFile;
   reader.readAsDataURL(file);
 }
 
-function processFile(event) {
-  var encodedFile = event.target.result;
-  sendFiletoCloudVision(encodedFile)
+/**
+ * Event handler for a file's data url - extract the image data and pass it off.
+ */
+function processFile (event) {
+  var content = event.target.result;
+  sendFileToCloudVision(content.replace('data:image/jpeg;base64,', ''));
 }
 
-var type = $("#type").val();
+/**
+ * Sends the given file contents to the Cloud Vision API and outputs the
+ * results.
+ */
+function sendFileToCloudVision (content) {
+  var type = $('#fileform [name=type]').val();
 
-// Strip out the file prefix when you convert to json.
-var json = '{' +
-  ' "requests": [' +
-  '	{ ' +
-  '	  "image": {' +
-  '	    "content":"' + content.replace("data:image/jpeg;base64,", "") + '"' +
-  '	  },' +
-  '	  "features": [' +
-  '	      {' +
-  '	      	"type": "' + type + '",' +
-  '			"maxResults": 200' +
-  '	      }' +
-  '	  ]' +
-  '	}' +
-  ']' +
-  '}';
+  // Strip out the file prefix when you convert to json.
+  var request = {
+    requests: [{
+      image: {
+        content: content
+      },
+      features: [{
+        type: type,
+        maxResults: 200
+      }]
+    }]
+  };
 
+  $('#results').text('Loading...');
+  $.post({
+    url: CV_URL,
+    data: JSON.stringify(request),
+    contentType: 'application/json'
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    $('#results').text('ERRORS: ' + textStatus + ' ' + errorThrown);
+  }).done(displayJSON);
+}
+
+/**
+ * Displays the results.
+ */
+function displayJSON (data) {
+  var contents = JSON.stringify(data, null, 4);
+  $('#results').text(contents);
+  var evt = new Event('results-displayed');
+  evt.results = contents;
+  document.dispatchEvent(evt);
+}
